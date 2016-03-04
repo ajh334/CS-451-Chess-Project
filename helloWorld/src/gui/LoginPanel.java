d0c327387d6e6b57620348f8cae40967f668aa9a
kfl29 <kfl29@drexel.edu>
2016-03-04 15:37:08 -0500

    implemented login function

--- helloWorld/src/gui/LoginPanel.java
+++ helloWorld/src/gui/LoginPanel.java
@@ -9,6 +9,8 @@ import java.sql.*;
 
 import javax.swing.*;
 import javax.swing.border.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import chess.ChessBoard;
 import chess.ChessColor;
@@ -24,6 +26,7 @@ public class LoginPanel extends JPanel
 	
 	static Color ochre = new Color(204,119,34);
 	private JLabel welcome;
+	private JLabel error;
 	private JTextField username;
 	private JButton login;
 	
@@ -87,10 +90,38 @@ public class LoginPanel extends JPanel
    addPic(picture, i);
 	
    JPanel option = new JPanel();
+   option.setLayout(new GridLayout(2,2));
    option.setBackground(ochre);
    add(option, BorderLayout.SOUTH);
    username = addText(option, 20, Color.BLACK, Color.WHITE);
+   username.getDocument().addDocumentListener(new DocumentListener() {
+	   public void changedUpdate(DocumentEvent e) {
+		    changed();
+		  }
+		  public void removeUpdate(DocumentEvent e) {
+		    changed();
+		  }
+		  public void insertUpdate(DocumentEvent e) {
+		    changed();
+		  }
+
+		  public void changed() {
+		     if (username.getText().equals("")){
+		       login.setEnabled(false);
+		     }
+		     else {
+		       login.setEnabled(true);
+		    }
+
+		  }
+		});
+   
    login = addButton(option, "Login", "Click here to login", new LoginListener());
+   login.setActionCommand("UnAuth");
+   login.setEnabled(false);
+   error = addLabel(option, " ");
+   error.setForeground(Color.RED);
+   error.setFont(new Font("Lucida Bright",Font.PLAIN, 12));
 }
  
  public JButton getLoginButton()
@@ -107,42 +138,104 @@ public class LoginPanel extends JPanel
  {
 	 return welcome;
  }
+ 
+ public Boolean checkUserName(Statement st, ResultSet rs, String usr, int ctr) throws SQLException
+ {
+	 if(!rs.next())
+	 {
+		 st.executeUpdate("insert into login(PlayerID, username)" + " values(" + ctr + ", \"" + usr + "\")");
+		 login.setActionCommand("Auth");
+		 System.out.println("login status: " + login.getActionCommand());
+		 return true;
+	 }
+	 String test = rs.getString("username");
+	 System.out.println("Found username: " + test);
+	 if(test.equals(usr))
+	 {
+		 System.out.println(usr + " already exists");
+		 rs.beforeFirst();
+		 return false;
+	 }
+	 else
+	 {
+		 ctr++;
+		 System.out.println("increment counter: " + ctr);
+		 return checkUserName(st, rs, usr, ctr);
+	 }
+ }
+ 
+ public static boolean isWhiteSpace(String str) {
+     if (str == null) {
+         return false;
+     }
+     int sz = str.length();
+     for (int i = 0; i < sz; i++) {
+         if ((Character.isWhitespace(str.charAt(i)) == false)) {
+             return false;
+         }
+     }
+     return true;
+ }
+
    
  private class LoginListener implements ActionListener
 {
     public void actionPerformed(ActionEvent e)
    {
+    	login.setActionCommand("UnAuth");
     	String user = username.getText();
+    	if(isWhiteSpace(user) || user == null)
+    	{
+    		error.setText("Username must contain some characters");
+    		return;
+    	}
     	System.out.println("Username pulled from textfield: " + user);
     	Connection con = null;
     	try {
-			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e1) {
 			// TODO Auto-generated catch block
 			System.out.println("Class not found");
 			e1.printStackTrace();
 		}
     	
-    	try {
-			con = DriverManager.getConnection("JDBC:ODBC:ChessChamp", "chesschamp", "chesschamp");			
+    	try 
+    	{
+			con = DriverManager.getConnection("jdbc:mysql://localhost/test", "chesschamp", "chesschamp");			
 			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("select * from username");
 			
-		} catch (SQLException e1) {
+			ResultSet rs = st.executeQuery("select username from login");
+			if (!rs.isBeforeFirst() ) 
+			{    
+				 System.out.println("No data"); 
+			} 
+			int i = 0;
+			
+			if(!checkUserName(st, rs, user, i))
+			{
+				error.setText("Username taken");
+				return;
+			}
+			else
+			{
+			error.setForeground(Color.GREEN);
+			error.setText("Authorized. Logging in...");
+			
+	    	ChessChamp.f.dispose();
+	    	frameMenu.setSize(805,525);
+	    	frameMenu.setLocationByPlatform(true);
+	    	frameMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    	frameMenu.setContentPane(new MenuGUI(user));
+			frameMenu.pack();
+			frameMenu.setVisible(true);
+			}
+		} 
+    	catch (SQLException e1) 
+    	{
 			System.out.println("Database error");
 			e1.printStackTrace();
 		}
-    	
 
-		
-    	ChessChamp.f.dispose();
-    	
-    	frameMenu.setSize(805,525);
-    	frameMenu.setLocationByPlatform(true);
-    	frameMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frameMenu.setContentPane(new MenuGUI(user));
-		frameMenu.pack();
-		frameMenu.setVisible(true);           
    }
 }
 }
